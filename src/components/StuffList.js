d4cb5c521c4aa18ec2fb7d81e6dbee22e18ccf0e
dodo <dmukunzi0@gmail.com>
2017-11-07 17:05:00 +0200

    setup axios and test with logs

--- src/components/StuffList.js
+++ src/components/StuffList.js
@@ -1,14 +1,21 @@
 // imports libraries for making a component
 import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import axios from 'axios';
 
 // making the component
 class StuffList extends Component {
     // const { } = styles;
+    componentWillMount() {
+        // alert('componentWillMount mounted!!!');
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => console.log(response));
+        // debugger;
+    }
     render() {
         return (
             <View>
-                <Text>Shtuff Lister</Text>
+                <Text>Good Shtuff Lister</Text>
             </View>
         );
     }
