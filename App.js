5e64102fc5da978e5617c5014fb693d778fab73e
dodo <dmukunzi0@gmail.com>
2017-11-07 15:42:14 +0200

    Make StuffList component a class-based component and return jsx with render

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/components/header';
+import { StyleSheet, View } from 'react-native';
+import Header from './src/components/Header';
+import StuffList from './src/components/StuffList';
 
 export default class App extends React.Component {
   render() {
     return (
+
+      // One Top Level Tag
       <View>
         <Header headerText={'goodSTUFF'} />
+        <StuffList />
       </View>
     );
   }
---
+++ src/components/StuffList.js
@@ -0,0 +1,23 @@
+// imports libraries for making a component
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+// making the component
+class StuffList extends Component {
+    // const { } = styles;
+    render() {
+        return (
+            <View>
+                <Text>Shtuff Lister</Text>
+            </View>
+        );
+    }
+}
+
+// Styles
+// const styles = {
+
+// };
+
+// exporting the component
+export default StuffList;
